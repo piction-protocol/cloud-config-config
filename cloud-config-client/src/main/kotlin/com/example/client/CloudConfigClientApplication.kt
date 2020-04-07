e19430e93d462a9a5a2cb85faf117618300d82eb
sk.kwon <sk.kwon@battleent.com>
2020-04-07 18:16:48 +0900

    Add refresh api

--- cloud-config-client/src/main/kotlin/com/example/client/CloudConfigClientApplication.kt
+++ cloud-config-client/src/main/kotlin/com/example/client/CloudConfigClientApplication.kt
@@ -3,9 +3,15 @@ package com.example.client
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+import javax.servlet.http.HttpServletRequest
 
 @SpringBootApplication
 class CloudConfigClientApplication
@@ -26,4 +32,17 @@ class SampleController {
 
     @GetMapping("/message")
     fun message() = SampleProperties.message
+}
+
+@RestController
+class RefreshController {
+
+    @PostMapping("/refresh")
+    fun refresh(request: HttpServletRequest) = RestTemplate().postForEntity(
+        request.requestURL.toString().replace(request.requestURI, "/actuator/bus-refresh"),
+        HttpEntity("", HttpHeaders().apply {
+            this.add(HttpHeaders.CONTENT_TYPE, "application/json")
+        }),
+        String::class.java
+    )
 }
