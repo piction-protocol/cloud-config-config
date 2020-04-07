f09a01d31f05bf9b06f69c2145e0fd16b12bd151
sk.kwon <sk.kwon@battleent.com>
2020-04-07 15:55:19 +0900

    Add message api

--- cloud-config-client/src/main/kotlin/com/example/client/CloudConfigClientApplication.kt
+++ cloud-config-client/src/main/kotlin/com/example/client/CloudConfigClientApplication.kt
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class CloudConfigClientApplication
@@ -17,4 +19,11 @@ fun main(args: Array<String>) {
 object SampleProperties {
     lateinit var message: String
     lateinit var encryptedMessage: String
+}
+
+@RestController
+class SampleController {
+
+    @GetMapping("/message")
+    fun message() = SampleProperties.message
 }
