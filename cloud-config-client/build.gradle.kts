dadc4076c7fe2f648aa10bbb29b5c8e0521b1f7b
sk.kwon <sk.kwon@battleent.com>
2020-04-07 15:55:06 +0900

    Implement bus event

--- cloud-config-client/build.gradle.kts
+++ cloud-config-client/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
